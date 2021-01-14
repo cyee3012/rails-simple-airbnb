51c308c3b1412afa8c4498a74e348bf161825e79
Chii Law <cyee3012@hotmail.co.uk>
2021-01-14 21:54:07 +0900

    can create new flats. also added bootstrap and simple forms

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,6 +4,7 @@ class FlatsController < ApplicationController
   end
 
   def new
+    @flat = Flat.new
   end
 
   def show
@@ -11,6 +12,13 @@ class FlatsController < ApplicationController
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,5 +28,11 @@ class FlatsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
